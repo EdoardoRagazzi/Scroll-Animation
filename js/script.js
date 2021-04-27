778381aa8b6a8e6773fbcfe59bb527c32b4b2f91
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-04-28 00:48:36 +0200

    scroll function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ const boxes = document.querySelectorAll('.box')
 
 window.addEventListener('Scroll', checkBoxes)
 
+checkBoxes()
+
 function checkBoxes() {
-    console.log(window.innerHeight)
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
